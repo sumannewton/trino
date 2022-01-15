bitsondatadev/presto

Add asRawJson decoding option for complex elasticsearch fields

plugin/trino-elasticsearch/src/main/java/io/trino/plugin/elasticsearch/BuiltinColumns.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static io.trino.plugin.elasticsearch.DecoderContext.primitiveDecoderContext;
 import static io.trino.spi.type.RealType.REAL;
 import static io.trino.spi.type.VarcharType.VARCHAR;
 import static java.util.Arrays.stream;
@@ -77,6 +78,10 @@ enum BuiltinColumns
 
     public ColumnHandle getColumnHandle()
     {
-        return new ElasticsearchColumnHandle(name, type, supportsPredicates);
+        return new ElasticsearchColumnHandle(
+                name,
+                type,
+                primitiveDecoderContext(name, type),
+                supportsPredicates);
     }
 }

plugin/trino-elasticsearch/src/main/java/io/trino/plugin/elasticsearch/ElasticsearchMetadata.java
@@ -25,6 +25,7 @@ import io.trino.plugin.elasticsearch.client.IndexMetadata;
 import io.trino.plugin.elasticsearch.client.IndexMetadata.DateTimeType;
 import io.trino.plugin.elasticsearch.client.IndexMetadata.ObjectType;
 import io.trino.plugin.elasticsearch.client.IndexMetadata.PrimitiveType;
+import io.trino.plugin.elasticsearch.decoders.DecoderType;
 import io.trino.spi.TrinoException;
 import io.trino.spi.connector.ColumnHandle;
 import io.trino.spi.connector.ColumnMetadata;
@@ -57,7 +58,11 @@ import java.util.OptionalLong;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static io.trino.plugin.elasticsearch.DecoderContext.ARRAY_ELEMENT_KEY;
+import static io.trino.plugin.elasticsearch.DecoderContext.primitiveDecoderContext;
 import static io.trino.plugin.elasticsearch.ElasticsearchTableHandle.Type.QUERY;
 import static io.trino.plugin.elasticsearch.ElasticsearchTableHandle.Type.SCAN;
 import static io.trino.spi.StandardErrorCode.INVALID_ARGUMENTS;
@@ -92,7 +97,11 @@ public class ElasticsearchMetadata
 
     private static final Map<String, ColumnHandle> PASSTHROUGH_QUERY_COLUMNS = ImmutableMap.of(
             PASSTHROUGH_QUERY_RESULT_COLUMN_NAME,
-            new ElasticsearchColumnHandle(PASSTHROUGH_QUERY_RESULT_COLUMN_NAME, VARCHAR, false));
+            new ElasticsearchColumnHandle(
+                    PASSTHROUGH_QUERY_RESULT_COLUMN_NAME,
+                    VARCHAR,
+                    primitiveDecoderContext(PASSTHROUGH_QUERY_RESULT_COLUMN_NAME, VARCHAR),
+                    false));
 
     private final Type ipAddressType;
     private final ElasticsearchClient client;
@@ -106,6 +115,31 @@ public class ElasticsearchMetadata
         this.client = requireNonNull(client, "client is null");
         requireNonNull(config, "config is null");
         this.schemaName = config.getDefaultSchema();
+    }
+
+    private DecoderContext getDecoderContext(IndexMetadata.Field field)
+    {
+        Type trinoType = toTrinoType(field);
+        if (field.asRawJson()) {
+            checkState(VARCHAR.equals(trinoType), "expected VARCHAR type for RawJson field");
+            return new DecoderContext(field.getName(), ImmutableMap.of(), DecoderType.RAW_JSON);
+        }
+
+        if (field.isArray()) {
+            checkState(trinoType instanceof ArrayType, "field is marked as an array, but Trino type is not ArrayType");
+
+            return new DecoderContext(
+                    field.getName(),
+                    ImmutableMap.of(ARRAY_ELEMENT_KEY, getDecoderContext(elementField(field))),
+                    DecoderType.ARRAY);
+        }
+        if (trinoType instanceof RowType) {
+            Map<String, DecoderContext> children = ((ObjectType) field.getType()).getFields().stream()
+                    .collect(toImmutableMap(IndexMetadata.Field::getName, this::getDecoderContext));
+            return new DecoderContext(field.getName(), children, DecoderType.ROW);
+        }
+
+        return primitiveDecoderContext(field.getName(), trinoType);
     }
 
     @Override
@@ -238,6 +272,7 @@ public class ElasticsearchMetadata
             result.put(field.getName(), new ElasticsearchColumnHandle(
                     field.getName(),
                     toTrinoType(field),
+                    getDecoderContext(field),
                     supportsPredicates(field.getType())));
         }
 
@@ -269,15 +304,15 @@ public class ElasticsearchMetadata
 
     private Type toTrinoType(IndexMetadata.Field metaDataField)
     {
-        return toTrinoType(metaDataField, metaDataField.isArray());
-    }
-
-    private Type toTrinoType(IndexMetadata.Field metaDataField, boolean isArray)
-    {
+        checkArgument(!metaDataField.asRawJson() || !metaDataField.isArray(), format("A column, (%s) cannot be declared as a Trino array and also be rendered as json.",
+                metaDataField.getName()));
         IndexMetadata.Type type = metaDataField.getType();
-        if (isArray) {
-            Type elementType = toTrinoType(metaDataField, false);
+        if (metaDataField.isArray()) {
+            Type elementType = toTrinoType(elementField(metaDataField));
             return new ArrayType(elementType);
+        }
+        if (metaDataField.asRawJson()) {
+            return VARCHAR;
         }
         if (type instanceof PrimitiveType) {
             switch (((PrimitiveType) type).getName()) {
@@ -331,6 +366,12 @@ public class ElasticsearchMetadata
         }
 
         return null;
+    }
+
+    public static IndexMetadata.Field elementField(IndexMetadata.Field field)
+    {
+        checkArgument(field.isArray(), "Cannot get element field from a non-array field");
+        return new IndexMetadata.Field(field.asRawJson(), false, field.getName(), field.getType());
     }
 
     @Override

plugin/trino-elasticsearch/src/main/java/io/trino/plugin/elasticsearch/ScanQueryPageSource.java
@@ -25,6 +25,7 @@ import io.trino.plugin.elasticsearch.decoders.DoubleDecoder;
 import io.trino.plugin.elasticsearch.decoders.IdColumnDecoder;
 import io.trino.plugin.elasticsearch.decoders.IntegerDecoder;
 import io.trino.plugin.elasticsearch.decoders.IpAddressDecoder;
+import io.trino.plugin.elasticsearch.decoders.RawJsonDecoder;
 import io.trino.plugin.elasticsearch.decoders.RealDecoder;
 import io.trino.plugin.elasticsearch.decoders.RowDecoder;
 import io.trino.plugin.elasticsearch.decoders.ScoreColumnDecoder;
@@ -39,10 +40,11 @@ import io.trino.spi.block.Block;
 import io.trino.spi.block.BlockBuilder;
 import io.trino.spi.block.PageBuilderStatus;
 import io.trino.spi.connector.ConnectorPageSource;
-import io.trino.spi.type.ArrayType;
 import io.trino.spi.type.RowType;
 import io.trino.spi.type.StandardTypes;
 import io.trino.spi.type.Type;
+import io.trino.spi.type.TypeManager;
+import io.trino.spi.type.TypeSignature;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
@@ -60,17 +62,9 @@ import static io.trino.plugin.elasticsearch.BuiltinColumns.ID;
 import static io.trino.plugin.elasticsearch.BuiltinColumns.SCORE;
 import static io.trino.plugin.elasticsearch.BuiltinColumns.SOURCE;
 import static io.trino.plugin.elasticsearch.BuiltinColumns.isBuiltinColumn;
+import static io.trino.plugin.elasticsearch.DecoderContext.ARRAY_ELEMENT_KEY;
 import static io.trino.plugin.elasticsearch.ElasticsearchQueryBuilder.buildSearchQuery;
-import static io.trino.spi.type.BigintType.BIGINT;
-import static io.trino.spi.type.BooleanType.BOOLEAN;
-import static io.trino.spi.type.DoubleType.DOUBLE;
-import static io.trino.spi.type.IntegerType.INTEGER;
-import static io.trino.spi.type.RealType.REAL;
-import static io.trino.spi.type.SmallintType.SMALLINT;
 import static io.trino.spi.type.TimestampType.TIMESTAMP_MILLIS;
-import static io.trino.spi.type.TinyintType.TINYINT;
-import static io.trino.spi.type.VarbinaryType.VARBINARY;
-import static io.trino.spi.type.VarcharType.VARCHAR;
 import static java.util.Objects.requireNonNull;
 import static java.util.function.Predicate.isEqual;
 import static java.util.stream.Collectors.toList;
@@ -85,19 +79,23 @@ public class ScanQueryPageSource
     private final SearchHitIterator iterator;
     private final BlockBuilder[] columnBuilders;
     private final List<ElasticsearchColumnHandle> columns;
+    private final Type ipAddressType;
     private long totalBytes;
     private long readTimeNanos;
 
     public ScanQueryPageSource(
             ElasticsearchClient client,
+            TypeManager typeManager,
             ElasticsearchTableHandle table,
             ElasticsearchSplit split,
             List<ElasticsearchColumnHandle> columns)
     {
         requireNonNull(client, "client is null");
+        requireNonNull(typeManager, "typeManager is null");
         requireNonNull(columns, "columns is null");
 
         this.columns = ImmutableList.copyOf(columns);
+        this.ipAddressType = typeManager.getType(new TypeSignature(StandardTypes.IPADDRESS));
 
         decoders = createDecoders(columns);
 
@@ -267,67 +265,61 @@ public class ScanQueryPageSource
                         return new SourceColumnDecoder();
                     }
 
-                    return createDecoder(column.getName(), column.getType());
+                    return createDecoder(column.getName(), column.getDecoderContext());
                 })
                 .collect(toImmutableList());
     }
 
-    private Decoder createDecoder(String path, Type type)
+    private Decoder createDecoder(String path, DecoderContext decoderContext)
     {
-        if (type.equals(VARCHAR)) {
-            return new VarcharDecoder(path);
-        }
-        if (type.equals(VARBINARY)) {
-            return new VarbinaryDecoder(path);
-        }
-        if (type.equals(TIMESTAMP_MILLIS)) {
-            return new TimestampDecoder(path);
-        }
-        if (type.equals(BOOLEAN)) {
-            return new BooleanDecoder(path);
-        }
-        if (type.equals(DOUBLE)) {
-            return new DoubleDecoder(path);
-        }
-        if (type.equals(REAL)) {
-            return new RealDecoder(path);
-        }
-        if (type.equals(TINYINT)) {
-            return new TinyintDecoder(path);
-        }
-        if (type.equals(SMALLINT)) {
-            return new SmallintDecoder(path);
-        }
-        if (type.equals(INTEGER)) {
-            return new IntegerDecoder(path);
-        }
-        if (type.equals(BIGINT)) {
-            return new BigintDecoder(path);
-        }
-        if (type.getBaseName().equals(StandardTypes.IPADDRESS)) {
-            return new IpAddressDecoder(path, type);
-        }
-        if (type instanceof RowType) {
-            RowType rowType = (RowType) type;
+        switch (decoderContext.getDecoderType()) {
+            case RAW_JSON:
+                return new RawJsonDecoder(path);
+            case VARCHAR:
+                return new VarcharDecoder(path);
+            case VARBINARY:
+                return new VarbinaryDecoder(path);
+            case TIMESTAMP:
+                return new TimestampDecoder(path);
+            case TIMESTAMP_WITH_TIME_ZONE:
+                throw new UnsupportedOperationException("Decoder type not supported: " + decoderContext.getDecoderType());
+            case BOOLEAN:
+                return new BooleanDecoder(path);
+            case DOUBLE:
+                return new DoubleDecoder(path);
+            case REAL:
+                return new RealDecoder(path);
+            case TINYINT:
+                return new TinyintDecoder(path);
+            case SMALLINT:
+                return new SmallintDecoder(path);
+            case INTEGER:
+                return new IntegerDecoder(path);
+            case BIGINT:
+                return new BigintDecoder(path);
+            case IP_ADDRESS:
+                return new IpAddressDecoder(path, ipAddressType);
+            case ROW:
+                Map<String, DecoderContext> children = decoderContext.getChildren();
 
-            List<Decoder> decoders = rowType.getFields().stream()
-                    .map(field -> createDecoder(appendPath(path, field.getName().get()), field.getType()))
-                    .collect(toImmutableList());
+                List<Decoder> decoders = children.values().stream()
+                        .map(child -> createDecoder(
+                                appendPath(path, child.getName()),
+                                child))
+                        .collect(toImmutableList());
 
-            List<String> fieldNames = rowType.getFields().stream()
-                    .map(RowType.Field::getName)
-                    .map(Optional::get)
-                    .collect(toImmutableList());
+                List<String> fieldNames = children.values().stream()
+                        .map(DecoderContext::getName)
+                        .collect(toImmutableList());
 
-            return new RowDecoder(path, fieldNames, decoders);
+                return new RowDecoder(path, fieldNames, decoders);
+            case ARRAY:
+                return new ArrayDecoder(createDecoder(
+                        path,
+                        decoderContext.getChildren().get(ARRAY_ELEMENT_KEY)));
+            default:
+                throw new UnsupportedOperationException("Decoder type not supported: " + decoderContext.getDecoderType());
         }
-        if (type instanceof ArrayType) {
-            Type elementType = ((ArrayType) type).getElementType();
-
-            return new ArrayDecoder(createDecoder(path, elementType));
-        }
-
-        throw new UnsupportedOperationException("Type not supported: " + type);
     }
 
     private static String appendPath(String base, String element)

sumannewton/trino

plugin/trino-elasticsearch/src/main/java/io/trino/plugin/elasticsearch/BuiltinColumns.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static io.trino.plugin.elasticsearch.DecoderDescriptor.primitiveDecoderDescriptor;
 import static io.trino.spi.type.RealType.REAL;
 import static io.trino.spi.type.VarcharType.VARCHAR;
 import static java.util.Arrays.stream;
@@ -77,6 +78,10 @@ enum BuiltinColumns
 
     public ColumnHandle getColumnHandle()
     {
-        return new ElasticsearchColumnHandle(name, type, supportsPredicates);
+        return new ElasticsearchColumnHandle(
+                name,
+                type,
+                primitiveDecoderDescriptor(name, type),
+                supportsPredicates);
     }
 }

plugin/trino-elasticsearch/src/main/java/io/trino/plugin/elasticsearch/ElasticsearchMetadata.java
@@ -25,6 +25,7 @@ import io.trino.plugin.elasticsearch.client.IndexMetadata;
 import io.trino.plugin.elasticsearch.client.IndexMetadata.DateTimeType;
 import io.trino.plugin.elasticsearch.client.IndexMetadata.ObjectType;
 import io.trino.plugin.elasticsearch.client.IndexMetadata.PrimitiveType;
+import io.trino.plugin.elasticsearch.decoders.DecoderType;
 import io.trino.spi.TrinoException;
 import io.trino.spi.connector.ColumnHandle;
 import io.trino.spi.connector.ColumnMetadata;
@@ -57,7 +58,11 @@ import java.util.OptionalLong;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static io.trino.plugin.elasticsearch.DecoderDescriptor.ARRAY_ELEMENT_KEY;
+import static io.trino.plugin.elasticsearch.DecoderDescriptor.primitiveDecoderDescriptor;
 import static io.trino.plugin.elasticsearch.ElasticsearchTableHandle.Type.QUERY;
 import static io.trino.plugin.elasticsearch.ElasticsearchTableHandle.Type.SCAN;
 import static io.trino.spi.StandardErrorCode.INVALID_ARGUMENTS;
@@ -92,7 +97,11 @@ public class ElasticsearchMetadata
 
     private static final Map<String, ColumnHandle> PASSTHROUGH_QUERY_COLUMNS = ImmutableMap.of(
             PASSTHROUGH_QUERY_RESULT_COLUMN_NAME,
-            new ElasticsearchColumnHandle(PASSTHROUGH_QUERY_RESULT_COLUMN_NAME, VARCHAR, false));
+            new ElasticsearchColumnHandle(
+                    PASSTHROUGH_QUERY_RESULT_COLUMN_NAME,
+                    VARCHAR,
+                    primitiveDecoderDescriptor(PASSTHROUGH_QUERY_RESULT_COLUMN_NAME, VARCHAR),
+                    false));
 
     private final Type ipAddressType;
     private final ElasticsearchClient client;
@@ -106,6 +115,31 @@ public class ElasticsearchMetadata
         this.client = requireNonNull(client, "client is null");
         requireNonNull(config, "config is null");
         this.schemaName = config.getDefaultSchema();
+    }
+
+    private DecoderDescriptor getDecoderDescriptor(IndexMetadata.Field field)
+    {
+        Type trinoType = toTrinoType(field);
+        if (field.asRawJson()) {
+            checkState(VARCHAR.equals(trinoType), "expected VARCHAR type for RawJson field");
+            return new DecoderDescriptor(field.getName(), ImmutableMap.of(), DecoderType.RAW_JSON);
+        }
+
+        if (field.isArray()) {
+            checkState(trinoType instanceof ArrayType, "field is marked as an array, but Trino type is not ArrayType");
+
+            return new DecoderDescriptor(
+                    field.getName(),
+                    ImmutableMap.of(ARRAY_ELEMENT_KEY, getDecoderDescriptor(elementField(field))),
+                    DecoderType.ARRAY);
+        }
+        if (trinoType instanceof RowType) {
+            Map<String, DecoderDescriptor> children = ((ObjectType) field.getType()).getFields().stream()
+                    .collect(toImmutableMap(IndexMetadata.Field::getName, this::getDecoderDescriptor));
+            return new DecoderDescriptor(field.getName(), children, DecoderType.ROW);
+        }
+
+        return primitiveDecoderDescriptor(field.getName(), trinoType);
     }
 
     @Override
@@ -238,6 +272,7 @@ public class ElasticsearchMetadata
             result.put(field.getName(), new ElasticsearchColumnHandle(
                     field.getName(),
                     toTrinoType(field),
+                    getDecoderDescriptor(field),
                     supportsPredicates(field.getType())));
         }
 
@@ -269,15 +304,15 @@ public class ElasticsearchMetadata
 
     private Type toTrinoType(IndexMetadata.Field metaDataField)
     {
-        return toTrinoType(metaDataField, metaDataField.isArray());
-    }
-
-    private Type toTrinoType(IndexMetadata.Field metaDataField, boolean isArray)
-    {
+        checkArgument(!metaDataField.asRawJson() || !metaDataField.isArray(), format("A column, (%s) cannot be declared as a Trino array and also be rendered as json.",
+                metaDataField.getName()));
         IndexMetadata.Type type = metaDataField.getType();
-        if (isArray) {
-            Type elementType = toTrinoType(metaDataField, false);
+        if (metaDataField.isArray()) {
+            Type elementType = toTrinoType(elementField(metaDataField));
             return new ArrayType(elementType);
+        }
+        if (metaDataField.asRawJson()) {
+            return VARCHAR;
         }
         if (type instanceof PrimitiveType) {
             switch (((PrimitiveType) type).getName()) {
@@ -331,6 +366,12 @@ public class ElasticsearchMetadata
         }
 
         return null;
+    }
+
+    public static IndexMetadata.Field elementField(IndexMetadata.Field field)
+    {
+        checkArgument(field.isArray(), "Cannot get element field from a non-array field");
+        return new IndexMetadata.Field(field.asRawJson(), false, field.getName(), field.getType());
     }
 
     @Override

plugin/trino-elasticsearch/src/main/java/io/trino/plugin/elasticsearch/ScanQueryPageSource.java
@@ -25,6 +25,7 @@ import io.trino.plugin.elasticsearch.decoders.DoubleDecoder;
 import io.trino.plugin.elasticsearch.decoders.IdColumnDecoder;
 import io.trino.plugin.elasticsearch.decoders.IntegerDecoder;
 import io.trino.plugin.elasticsearch.decoders.IpAddressDecoder;
+import io.trino.plugin.elasticsearch.decoders.RawJsonDecoder;
 import io.trino.plugin.elasticsearch.decoders.RealDecoder;
 import io.trino.plugin.elasticsearch.decoders.RowDecoder;
 import io.trino.plugin.elasticsearch.decoders.ScoreColumnDecoder;
@@ -43,6 +44,8 @@ import io.trino.spi.type.ArrayType;
 import io.trino.spi.type.RowType;
 import io.trino.spi.type.StandardTypes;
 import io.trino.spi.type.Type;
+import io.trino.spi.type.TypeManager;
+import io.trino.spi.type.TypeSignature;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
@@ -60,17 +63,9 @@ import static io.trino.plugin.elasticsearch.BuiltinColumns.ID;
 import static io.trino.plugin.elasticsearch.BuiltinColumns.SCORE;
 import static io.trino.plugin.elasticsearch.BuiltinColumns.SOURCE;
 import static io.trino.plugin.elasticsearch.BuiltinColumns.isBuiltinColumn;
+import static io.trino.plugin.elasticsearch.DecoderDescriptor.ARRAY_ELEMENT_KEY;
 import static io.trino.plugin.elasticsearch.ElasticsearchQueryBuilder.buildSearchQuery;
-import static io.trino.spi.type.BigintType.BIGINT;
-import static io.trino.spi.type.BooleanType.BOOLEAN;
-import static io.trino.spi.type.DoubleType.DOUBLE;
-import static io.trino.spi.type.IntegerType.INTEGER;
-import static io.trino.spi.type.RealType.REAL;
-import static io.trino.spi.type.SmallintType.SMALLINT;
 import static io.trino.spi.type.TimestampType.TIMESTAMP_MILLIS;
-import static io.trino.spi.type.TinyintType.TINYINT;
-import static io.trino.spi.type.VarbinaryType.VARBINARY;
-import static io.trino.spi.type.VarcharType.VARCHAR;
 import static java.util.Objects.requireNonNull;
 import static java.util.function.Predicate.isEqual;
 import static java.util.stream.Collectors.toList;
@@ -85,19 +80,23 @@ public class ScanQueryPageSource
     private final SearchHitIterator iterator;
     private final BlockBuilder[] columnBuilders;
     private final List<ElasticsearchColumnHandle> columns;
+    private final Type ipAddressType;
     private long totalBytes;
     private long readTimeNanos;
 
     public ScanQueryPageSource(
             ElasticsearchClient client,
+            TypeManager typeManager,
             ElasticsearchTableHandle table,
             ElasticsearchSplit split,
             List<ElasticsearchColumnHandle> columns)
     {
         requireNonNull(client, "client is null");
+        requireNonNull(typeManager, "typeManager is null");
         requireNonNull(columns, "columns is null");
 
         this.columns = ImmutableList.copyOf(columns);
+        this.ipAddressType = typeManager.getType(new TypeSignature(StandardTypes.IPADDRESS));
 
         decoders = createDecoders(columns);
 
@@ -267,67 +266,65 @@ public class ScanQueryPageSource
                         return new SourceColumnDecoder();
                     }
 
-                    return createDecoder(column.getName(), column.getType());
+                    return createDecoder(column.getName(), column.getType(), column.getDecoderDescriptor());
                 })
                 .collect(toImmutableList());
     }
 
-    private Decoder createDecoder(String path, Type type)
+    private Decoder createDecoder(String path, Type type, DecoderDescriptor decoderDescriptor)
     {
-        if (type.equals(VARCHAR)) {
-            return new VarcharDecoder(path);
-        }
-        if (type.equals(VARBINARY)) {
-            return new VarbinaryDecoder(path);
-        }
-        if (type.equals(TIMESTAMP_MILLIS)) {
-            return new TimestampDecoder(path);
-        }
-        if (type.equals(BOOLEAN)) {
-            return new BooleanDecoder(path);
-        }
-        if (type.equals(DOUBLE)) {
-            return new DoubleDecoder(path);
-        }
-        if (type.equals(REAL)) {
-            return new RealDecoder(path);
-        }
-        if (type.equals(TINYINT)) {
-            return new TinyintDecoder(path);
-        }
-        if (type.equals(SMALLINT)) {
-            return new SmallintDecoder(path);
-        }
-        if (type.equals(INTEGER)) {
-            return new IntegerDecoder(path);
-        }
-        if (type.equals(BIGINT)) {
-            return new BigintDecoder(path);
-        }
-        if (type.getBaseName().equals(StandardTypes.IPADDRESS)) {
-            return new IpAddressDecoder(path, type);
-        }
-        if (type instanceof RowType) {
-            RowType rowType = (RowType) type;
+        switch (decoderDescriptor.getDecoderType()) {
+            case RAW_JSON:
+                return new RawJsonDecoder(path);
+            case VARCHAR:
+                return new VarcharDecoder(path);
+            case VARBINARY:
+                return new VarbinaryDecoder(path);
+            case TIMESTAMP:
+                return new TimestampDecoder(path);
+            case TIMESTAMP_WITH_TIME_ZONE:
+                throw new UnsupportedOperationException("Decoder type not supported: " + decoderDescriptor.getDecoderType());
+            case BOOLEAN:
+                return new BooleanDecoder(path);
+            case DOUBLE:
+                return new DoubleDecoder(path);
+            case REAL:
+                return new RealDecoder(path);
+            case TINYINT:
+                return new TinyintDecoder(path);
+            case SMALLINT:
+                return new SmallintDecoder(path);
+            case INTEGER:
+                return new IntegerDecoder(path);
+            case BIGINT:
+                return new BigintDecoder(path);
+            case IP_ADDRESS:
+                return new IpAddressDecoder(path, ipAddressType);
+            case ROW:
+                Map<String, DecoderDescriptor> children = decoderDescriptor.getChildren();
+                RowType rowType = (RowType) type;
 
-            List<Decoder> decoders = rowType.getFields().stream()
-                    .map(field -> createDecoder(appendPath(path, field.getName().get()), field.getType()))
-                    .collect(toImmutableList());
+                List<Decoder> decoders = rowType.getFields().stream()
+                        .map(field -> createDecoder(
+                                appendPath(path, field.getName().get()),
+                                field.getType(),
+                                children.get(field.getName().get())))
+                        .collect(toImmutableList());
 
-            List<String> fieldNames = rowType.getFields().stream()
-                    .map(RowType.Field::getName)
-                    .map(Optional::get)
-                    .collect(toImmutableList());
+                List<String> fieldNames = rowType.getFields().stream()
+                        .map(RowType.Field::getName)
+                        .map(Optional::get)
+                        .collect(toImmutableList());
 
-            return new RowDecoder(path, fieldNames, decoders);
+                return new RowDecoder(path, fieldNames, decoders);
+            case ARRAY:
+                return new ArrayDecoder(createDecoder(
+                        path,
+                        ((ArrayType) type).getElementType(),
+                        decoderDescriptor.getChildren().get(ARRAY_ELEMENT_KEY)));
+            default:
+                throw new UnsupportedOperationException("Decoder type not supported: " + decoderDescriptor.getDecoderType());
         }
-        if (type instanceof ArrayType) {
-            Type elementType = ((ArrayType) type).getElementType();
-
-            return new ArrayDecoder(createDecoder(path, elementType));
-        }
-
-        throw new UnsupportedOperationException("Type not supported: " + type);
     }
 
     private static String appendPath(String base, String element)