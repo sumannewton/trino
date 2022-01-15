ebyhr/presto-1

Support pushdown for date, tinyint and smallint types in Cassandra

presto-cassandra/src/main/java/io/prestosql/plugin/cassandra/CassandraType.java
@@ -57,6 +57,7 @@ import static io.prestosql.spi.type.VarcharType.createUnboundedVarcharType;
 import static io.prestosql.spi.type.VarcharType.createVarcharType;
 import static java.lang.Float.floatToRawIntBits;
 import static java.lang.Float.intBitsToFloat;
+import static java.lang.Math.toIntExact;
 import static java.util.Objects.requireNonNull;
 
 public enum CassandraType
@@ -302,7 +303,7 @@ public enum CassandraType
             case TIMESTAMP:
                 return Long.toString(row.getTimestamp(position).getTime());
             case DATE:
-                return row.getDate(position).toString();
+                return quoteStringLiteral(row.getDate(position).toString());
             case INET:
                 return quoteStringLiteral(toAddrString(row.getInet(position)));
             case VARINT:
@@ -318,6 +319,10 @@ public enum CassandraType
     // TODO unify with getColumnValueForCql
     public String toCqlLiteral(Object prestoNativeValue)
     {
+        if (this == DATE) {
+            LocalDate date = LocalDate.fromDaysSinceEpoch(toIntExact((long) prestoNativeValue));
+            return quoteStringLiteral(date.toString());
+        }
         if (this == TIMESTAMP) {
             return String.valueOf(unpackMillisUtc((Long) prestoNativeValue));
         }
@@ -440,9 +445,12 @@ public enum CassandraType
             case BOOLEAN:
             case DOUBLE:
             case INET:
+            case TINYINT:
+            case SMALLINT:
             case INT:
             case FLOAT:
             case DECIMAL:
+            case DATE:
             case TIMESTAMP:
             case UUID:
             case TIMEUUID:

presto-cassandra/src/test/java/io/prestosql/plugin/cassandra/TestCassandraConnector.java
@@ -16,6 +16,8 @@ package io.prestosql.plugin.cassandra;
 import com.datastax.driver.core.utils.Bytes;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.primitives.Shorts;
+import com.google.common.primitives.SignedBytes;
 import io.prestosql.spi.connector.ColumnHandle;
 import io.prestosql.spi.connector.ColumnMetadata;
 import io.prestosql.spi.connector.Connector;
@@ -34,6 +36,7 @@ import io.prestosql.spi.connector.RecordCursor;
 import io.prestosql.spi.connector.SchemaNotFoundException;
 import io.prestosql.spi.connector.SchemaTableName;
 import io.prestosql.spi.connector.SchemaTablePrefix;
+import io.prestosql.spi.type.DateType;
 import io.prestosql.spi.type.Type;
 import io.prestosql.spi.type.VarcharType;
 import io.prestosql.testing.TestingConnectorContext;
@@ -60,9 +63,12 @@ import static io.prestosql.spi.type.DateTimeEncoding.packDateTimeWithZone;
 import static io.prestosql.spi.type.DoubleType.DOUBLE;
 import static io.prestosql.spi.type.IntegerType.INTEGER;
 import static io.prestosql.spi.type.RealType.REAL;
+import static io.prestosql.spi.type.SmallintType.SMALLINT;
 import static io.prestosql.spi.type.TimeZoneKey.UTC_KEY;
 import static io.prestosql.spi.type.TimestampWithTimeZoneType.TIMESTAMP_WITH_TIME_ZONE;
+import static io.prestosql.spi.type.TinyintType.TINYINT;
 import static io.prestosql.spi.type.VarbinaryType.VARBINARY;
+import static java.lang.Math.toIntExact;
 import static java.lang.String.format;
 import static java.util.Locale.ENGLISH;
 import static org.testng.Assert.assertEquals;
@@ -213,11 +219,20 @@ public class TestCassandraConnector
                 if (BOOLEAN.equals(type)) {
                     cursor.getBoolean(columnIndex);
                 }
+                else if (TINYINT.equals(type)) {
+                    SignedBytes.checkedCast(cursor.getLong(columnIndex));
+                }
+                else if (SMALLINT.equals(type)) {
+                    Shorts.checkedCast(cursor.getLong(columnIndex));
+                }
                 else if (INTEGER.equals(type)) {
-                    cursor.getLong(columnIndex);
+                    toIntExact(cursor.getLong(columnIndex));
                 }
                 else if (BIGINT.equals(type)) {
                     cursor.getLong(columnIndex);
+                }
+                else if (DateType.DATE.equals(type)) {
+                    toIntExact(cursor.getLong(columnIndex));
                 }
                 else if (TIMESTAMP_WITH_TIME_ZONE.equals(type)) {
                     cursor.getLong(columnIndex);

sumannewton/trino

plugin/trino-cassandra/src/main/java/io/trino/plugin/cassandra/CassandraType.java
@@ -57,6 +57,7 @@ import static io.trino.spi.type.VarcharType.createUnboundedVarcharType;
 import static io.trino.spi.type.VarcharType.createVarcharType;
 import static java.lang.Float.floatToRawIntBits;
 import static java.lang.Float.intBitsToFloat;
+import static java.lang.Math.toIntExact;
 import static java.util.Objects.requireNonNull;
 
 public enum CassandraType
@@ -301,7 +302,7 @@ public enum CassandraType
             case TIMESTAMP:
                 return Long.toString(row.getTimestamp(position).getTime());
             case DATE:
-                return row.getDate(position).toString();
+                return quoteStringLiteral(row.getDate(position).toString());
             case INET:
                 return quoteStringLiteral(toAddrString(row.getInet(position)));
             case VARINT:
@@ -322,6 +323,10 @@ public enum CassandraType
     // TODO unify with getColumnValueForCql
     public String toCqlLiteral(Object trinoNativeValue)
     {
+        if (this == DATE) {
+            LocalDate date = LocalDate.fromDaysSinceEpoch(toIntExact((long) trinoNativeValue));
+            return quoteStringLiteral(date.toString());
+        }
         if (this == TIMESTAMP) {
             return String.valueOf(unpackMillisUtc((Long) trinoNativeValue));
         }
@@ -443,8 +448,11 @@ public enum CassandraType
             case DOUBLE:
             case INET:
             case INT:
+            case TINYINT:
+            case SMALLINT:
             case FLOAT:
             case DECIMAL:
+            case DATE:
             case TIMESTAMP:
             case UUID:
             case TIMEUUID:

plugin/trino-cassandra/src/test/java/io/trino/plugin/cassandra/TestCassandraConnector.java
@@ -16,6 +16,8 @@ package io.trino.plugin.cassandra;
 import com.datastax.driver.core.utils.Bytes;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
+import com.google.common.primitives.Shorts;
+import com.google.common.primitives.SignedBytes;
 import io.trino.spi.connector.ColumnHandle;
 import io.trino.spi.connector.ColumnMetadata;
 import io.trino.spi.connector.Connector;
@@ -36,6 +38,7 @@ import io.trino.spi.connector.SchemaTableName;
 import io.trino.spi.connector.SchemaTablePrefix;
 import io.trino.spi.predicate.Domain;
 import io.trino.spi.predicate.TupleDomain;
+import io.trino.spi.type.DateType;
 import io.trino.spi.type.Type;
 import io.trino.spi.type.VarcharType;
 import io.trino.testing.TestingConnectorContext;
@@ -63,9 +66,12 @@ import static io.trino.spi.type.DateTimeEncoding.packDateTimeWithZone;
 import static io.trino.spi.type.DoubleType.DOUBLE;
 import static io.trino.spi.type.IntegerType.INTEGER;
 import static io.trino.spi.type.RealType.REAL;
+import static io.trino.spi.type.SmallintType.SMALLINT;
 import static io.trino.spi.type.TimeZoneKey.UTC_KEY;
 import static io.trino.spi.type.TimestampWithTimeZoneType.TIMESTAMP_WITH_TIME_ZONE;
+import static io.trino.spi.type.TinyintType.TINYINT;
 import static io.trino.spi.type.VarbinaryType.VARBINARY;
+import static java.lang.Math.toIntExact;
 import static java.lang.String.format;
 import static java.util.Locale.ENGLISH;
 import static org.testng.Assert.assertEquals;
@@ -235,11 +241,20 @@ public class TestCassandraConnector
                 if (BOOLEAN.equals(type)) {
                     cursor.getBoolean(columnIndex);
                 }
+                else if (TINYINT.equals(type)) {
+                    SignedBytes.checkedCast(cursor.getLong(columnIndex));
+                }
+                else if (SMALLINT.equals(type)) {
+                    Shorts.checkedCast(cursor.getLong(columnIndex));
+                }
                 else if (INTEGER.equals(type)) {
-                    cursor.getLong(columnIndex);
+                    toIntExact(cursor.getLong(columnIndex));
                 }
                 else if (BIGINT.equals(type)) {
                     cursor.getLong(columnIndex);
+                }
+                else if (DateType.DATE.equals(type)) {
+                    toIntExact(cursor.getLong(columnIndex));
                 }
                 else if (TIMESTAMP_WITH_TIME_ZONE.equals(type)) {
                     cursor.getLong(columnIndex);